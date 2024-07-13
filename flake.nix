c30f016329c8ab9e6152cf1c691a0634277c1b15
OuthBack <riquessan@gmail.com>
2024-07-13 17:34:11 -0300

    update

--- flake.nix
+++ flake.nix
@@ -5,21 +5,6 @@
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
 
-  outputs = { self, nixpkgs }: {
-      var = let
-          pkgs = (import <nixpkgs> {});
-      in pkgs.runCommand 
-          "home-manager-files"
-          {
-              nativeBuildInputs = [ pkgs.xorg.lndir ];
-          }
-          ''
-          touch /etc/host/extra/kitty/sexoooooooo
-          '';
-
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
+  outputs = { self }: {
   };
 }
